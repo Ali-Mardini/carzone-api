e836b91839748b0340312bc67f93fa8406064e00
Ali Mardini <mardinia6@gmail.com>
2021-08-14 17:51:36 +0400

    add Cors to startup file

--- carzone-api/Startup.cs
+++ carzone-api/Startup.cs
@@ -25,7 +25,7 @@ namespace carzone_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen();
         }
@@ -48,6 +48,13 @@ namespace carzone_api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
 
             app.UseAuthorization();
 
