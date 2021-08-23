7f103498fdedb14de858ed4f36e1f56619045109
Ali Mardini <mardinia6@gmail.com>
2021-08-24 01:10:27 +0400

    create login api

--- carzone-api/Controllers/BarberController.cs
+++ carzone-api/Controllers/BarberController.cs
@@ -53,5 +53,45 @@ namespace carzone_api.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDTO userPayload)
+        {
+            try
+            {
+                var jsonData = System.IO.File.ReadAllText("barberApi.json");
+
+                if (!String.IsNullOrEmpty(jsonData))
+                {
+                    var usersList = JsonConvert.DeserializeObject<List<UserDTO>>(jsonData);
+
+                    string hashedPassword = _passwordHasher.HashPassword(userPayload.Password);
+                    var isValidPassword = _passwordHasher.VerifyHashedPassword(hashedPassword, userPayload.Password);
+
+                    var user = (from u in usersList
+                               where u.Email == userPayload.Username &&
+                                     isValidPassword.ToString() == "Success"
+                                select u).FirstOrDefault();
+
+                    if (user != null)
+                    {
+                        return Ok(new { success = true, response = new {user.Email, message = "You have loggedin successfully!"} });
+                    }
+                    else
+                    {
+                        return NotFound(new { success = true, response = "Invalid username or password!" });
+                    }
+                    
+                }
+                else
+                {
+                    return NotFound(new { success = true, response = "Invalid username or password!" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
---
+++ carzone-api/Models/LoginDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace carzone_api.Models
+{
+    public class LoginDTO
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
